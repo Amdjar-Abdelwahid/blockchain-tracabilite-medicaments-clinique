6b77e32e9043a172207f288cd6b1842ff2fcc40e
Amdjar-Abdelwahid <amdjarabdelwahid1@gmail.com>
2025-12-04 23:38:59 +0100

    add sowager

---
+++ src/main/java/com/myorg/tracemed/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.myorg.tracemed.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI tracemedOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("TraceMed API")
+                        .description("API de traçabilité des médicaments avec blockchain interne")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
